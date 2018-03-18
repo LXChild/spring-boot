413b863ebbfb1c0ef50503f6b9e93ddb106498f6
WangChong <lxchild@foxmail.com>
2018-03-18 21:05:43 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 MYIMAGE=lxchild/springboot2_0
-
+MY_CONTAINER=lxchild
 # uncomment if you need push
 #docker login 192.168.1.2:8082 -u admin -p admin123
 
 # stop all container
-docker kill $(docker ps -aq)
-
+if [docker ps -a | grep -i ${MY_CONTAINER}]
+then
+    docker kill ${MY_CONTAINER}
+fi
 # remove all container
-docker rm $(docker ps -aq)
+docker rm ${MY_CONTAINER}
 
 # remove old images
-docker images | grep ${MYIMAGE} | awk '{print $3}' | xargs docker rmi
+docker images | grep ${MYIMAGE} | awk '{print $3}' | xargs docker rmi -f
 
 # build jar and image
 mvn clean package -e -X docker:build -DskipTest
 
 # running container
-docker run -dp 8080:8080 --name ${MYIMAGE} ${MYIMAGE}
+docker run -it -dp 8080:8080 --name ${MY_CONTAINER} ${MYIMAGE}
 
 # push image
-#docker push ${MYIMAGE}
+# docker push ${MYIMAGE}
