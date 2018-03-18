e5f7aefc9ddd61aa537a251f2163844de97f7b8a
WangChong <lxchild@foxmail.com>
2018-03-18 21:50:23 +0800

    update shell script

--- build.sh
+++ build.sh
@@ -5,21 +5,21 @@ MY_CONTAINER=lxchild
 # uncomment if you need push
 #docker login 192.168.1.2:8082 -u admin -p admin123
 
-# stop all container
+echo "stop all container"
 if docker ps -a | grep -i ${MY_CONTAINER}
 then
     docker kill ${MY_CONTAINER}
 fi
-# remove all container
+echo "remove all container"
 docker rm ${MY_CONTAINER}
 
-# remove old images
+echo "remove old images"
 docker images | grep ${MYIMAGE} | awk '{print $3}' | xargs docker rmi -f
 
-# build jar and image
+echo "build jar and image"
 mvn clean package -e -X docker:build -DskipTest
 
-# running container
+echo "running container"
 docker run -it -dp 18080:18080 --name ${MY_CONTAINER} ${MYIMAGE}
 
 # push image
